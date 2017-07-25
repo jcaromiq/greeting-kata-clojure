8a99f7e280b5001a3da5a945d821670b3a67d477
Joaquin Caro <me@joaquin-caro.es>
2017-07-26 00:13:53 +0200

    Green: greet shouting if name is in upper

--- src/es/joaquincaro/katas/Greeting.clj
+++ src/es/joaquincaro/katas/Greeting.clj
@@ -1,6 +1,11 @@
 (ns es.joaquincaro.katas.Greeting)
 
-(defn greet
-      [name]
-       (str "Hello, " name ".")
-      )
+(defn upper-case? [name] (and (> (count name) 0) (every? #(Character/isUpperCase %) name)))
+
+(defmulti greet (fn [name] [(nil? name) (upper-case? name)]) )
+
+(defmethod greet [true false] [_] "Hello, my friend.")
+(defmethod greet [false false] [n] ( str "Hello, " n "."))
+
+(defmethod greet [true true] [_] "HELLO, MY FRIEND.")
+(defmethod greet [false true] [n] ( str "HELLO, "(clojure.string/upper-case n)  "."))
--- test/es/joaquincaro/katas/GreetingShould.clj
+++ test/es/joaquincaro/katas/GreetingShould.clj
@@ -1,15 +1,20 @@
 (ns es.joaquincaro.katas.GreetingShould
     (:require
-        [midje.sweet :refer :all]
-        [es.joaquincaro.katas.Greeting :refer :all]))
+      [midje.sweet :refer :all]
+      [es.joaquincaro.katas.Greeting :refer :all]))
 
 (facts
   "About The Greeting Kata"
-  (fact
-    "Greet given a name\n"
-    (greet "Bob") => "Hello, Bob."
 
-    )
+  (fact "Greet given a name"
+        (greet "Bob") => "Hello, Bob.")
 
-  )
+  (fact "Greet with stand-it if name is null"
+        (greet nil) => "Hello, my friend.")
+
+  (fact "Greet with upper if name is in upper"
+        (greet "PEPE") => "HELLO, PEPE.")
+
+
+)
 
