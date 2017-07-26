e3cf19af688a51896d2d86fc28a111da18caaef5
Joaquin Caro <me@joaquin-caro.es>
2017-07-27 00:27:00 +0200

    Refactor: remove upperCase check in multimethod

--- src/es/joaquincaro/katas/Greeting.clj
+++ src/es/joaquincaro/katas/Greeting.clj
@@ -2,13 +2,17 @@
 
 (defn upper-case? [name] (and (> (count name) 0) (every? #(Character/isUpperCase %) name)))
 
-(defmulti greet (fn [name] [(nil? name) (upper-case? name)]) )
+(defn personalized-greeting [name] (str "Hello, " name "."))
 
 (defn anonymous-greeting [] (personalized-greeting "my friend"))
 
-(defmethod greet [true false] [_] (anonymous-greeting))
+(defmulti greet (fn [name] (nil? name)) )
 
-(defn personalized-greeting [name] (str "Hello, " name "."))
-(defmethod greet [false false] [name] (personalized-greeting name))
+(defmethod greet true [_] (anonymous-greeting))
 
-(defmethod greet [false true] [name] ( (comp clojure.string/upper-case personalized-greeting) name))
+
+(defmethod greet false
+           [name]
+           (if (upper-case? name)
+             ((comp clojure.string/upper-case personalized-greeting) name)
+             (personalized-greeting name)))
